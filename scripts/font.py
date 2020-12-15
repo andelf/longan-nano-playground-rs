6f9e82a0466d635b067c557344cc8152e8458230
Andelf <andelf@gmail.com>
2020-12-16 02:03:08 +0800

    feat: update font.py

--- scripts/font.py
+++ scripts/font.py
@@ -1,42 +1,55 @@
-
 from PIL import Image, ImageDraw, ImageFont
+import PIL.features
+
+print('libraqm:', PIL.features.check('raqm'))
 
 
 size = (320, 16)
 
+"""
 FORMAT = "RGB"
 BG = (255, 255, 255)
 FG = (0, 0, 0)
+"""
 
 FORMAT = '1'
-BG= 0
-FG=1
+BG = 0
+FG = 1
+
+YOFF = 0  # or -1
 
 
-CHARS = "卧槽艹牛逼气温湿度"
+CHARS = "气温湿度照压卧槽艹牛逼数据"
 
 
 im = Image.new(FORMAT, size, BG)
 
-font = ImageFont.truetype("sarasa-mono-sc-nerd-regular.ttf", size=16, index=0)
-# get a drawing context
+# font = ImageFont.truetype("sarasa-mono-sc-nerd-light.ttf", size=16, index=0)
+# font = ImageFont.truetype("sarasa-mono-sc-nerd-regular.ttf", size=16, index=0)
+font = ImageFont.truetype("Unibit.ttf", size=16, index=0)
+# font = ImageFont.truetype("zpix.ttf", size=12, index=0)
+
+
 draw = ImageDraw.Draw(im)
 
+draw.text((0, YOFF), CHARS, font=font, fill=FG, language='zh-CN')
+im.save('font.png')
+im.show()
 
-# draw.text((0,-1), CHARS, font=font, fill=FG)
-# draw text, full opacity
-# d.text((10,60), "World", font=fnt, fill=(255,255,255,255))
+
+draw.rectangle([(0, 0), size], fill=BG)
 
 
-ASCII = '\x00\x01\x02\x03\x04\x05\x06\x07\x08\t\n\x0b\x0c\r\x0e\x0f\x10\x11\x12\x13\x14\x15\x16\x17\x18\x19\x1a\x1b\x1c\x1d\x1e\x1f !"#$%&\'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~\x7f'
+# NOTE, char 127 is replaced with '°'
+ASCII = '\x00\x01\x02\x03\x04\x05\x06\x07\x08\t\n\x0b\x0c\r\x0e\x0f\x10\x11\x12\x13\x14\x15\x16\x17\x18\x19\x1a\x1b\x1c\x1d\x1e\x1f !"#$%&\'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~°'
 
 charmap = []
 
 for i, c in enumerate(ASCII):
     if c.isprintable():
-        draw.text((0,-1), c, font=font, fill=FG)
+        draw.text((0, YOFF), c, font=font, fill=FG)
     else:
-        draw.text((0,-1), "  ", font=font, fill=FG)
+        draw.text((0, YOFF), "  ", font=font, fill=FG)
 
     for y in range(16):
         v = 0
@@ -50,10 +63,10 @@ for i, c in enumerate(ASCII):
 
 # ascii done
 
-print(len(charmap))
+# print(len(charmap))
 
 for i, c in enumerate(CHARS):
-    draw.text((0,-1), c, font=font, fill=FG)
+    draw.text((0, YOFF), c, font=font, fill=FG)
 
     for y in range(16):
         v = 0
@@ -62,14 +75,10 @@ for i, c in enumerate(CHARS):
             v = (v << 1) + b
 
         charmap.append(v >> 8)
-        charmap.append(v & 0xff)
+        charmap.append(v & 0xFF)
 
     draw.rectangle([(0, 0), size], fill=BG)
 
 
-
-with open('font.raw', 'wb')  as fp:
+with open('font.raw', 'wb') as fp:
     fp.write(bytes(charmap))
-
-im.save('out.png')
-# im.show()
